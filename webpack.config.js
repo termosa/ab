06ad5da34436fef45878863ad4f9883b294be89f
Stanislav Termosa <termosa.stanislav@gmail.com>
2016-08-19 08:00:54 +0300

    Refactor Webpack config with webpack-manager

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
 var path = require('path');
 var webpack = require('webpack');
+var config = require('webpack-manager')();
 
 // Environment specification
 var IS_PROD = !~['dev', 'development'].indexOf(process.env.NODE_ENV);
 var IS_DEV = !IS_PROD;
 
-module.exports = {
-  context: './src',
-  entry: {
-    index: './index.js'
-  },
-  output: {
-    path: path.join(__dirname, '/app'),
-    filename: '[name].js'
-  },
-  resolve: {
-    alias: {
-      module: './modules'
-    }
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      IS_DEV: JSON.stringify(IS_DEV),
-      IS_PROD: JSON.stringify(IS_PROD)
-    })
-  ],
-  devtool: IS_DEV ? 'eval' : null
-};
+// Shorthands
+var alias = config.resolve.alias;
+var plugin = config.plugins.add;
+
+config.context('./src');
+config.entry('index', './index.js');
+
+config.output({
+  path: path.join(__dirname, '/app'),
+  filename: '[name].js'
+});
+
+config.devtool(IS_DEV ? 'eval' : null);
+
+alias('module', './modules');
+
+plugin(function() {
+  return new webpack.DefinePlugin({
+    IS_DEV: JSON.stringify(IS_DEV),
+    IS_PROD: JSON.stringify(IS_PROD)
+  });
+}());
+
+module.exports = config.setup();
