18a9330420d4c902133493768e59d0f931ac810d
Stanislav Termosa <termosa.stanislav@gmail.com>
2016-08-19 16:22:50 +0300

    Add 'window' and 'document' as modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var angular = require('angular');
 var appModule = require('component/app');
+var document = require('module/document');
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, [appModule.id]);
