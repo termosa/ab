66ca4be25a4233cdd45f85d4bdd9789d439c046d
Stanislav Termosa <termosa.stanislav@gmail.com>
2016-08-23 19:16:09 +0300

    Split UglifyJS options to a separate file

--- config/webpack-config/js-minification.js
+++ config/webpack-config/js-minification.js
@@ -1,28 +1,8 @@
+var path = require('path');
 var UglifyJsPlugin = require('webpack').optimize.UglifyJsPlugin;
 
-var uglifyOptions = {
-  compress: {
-    unsafe: false,
-    warnings: false,
-    passes: 2,
-
-    sequences: true,
-    properties: true,
-    dead_code: true,
-    drop_debugger: true,
-    conditionals: true,
-    comparisons: true,
-    evaluate: true,
-    booleans: true,
-    loops: true,
-    if_return: true,
-    join_vars: true,
-    cascade: true,
-    negate_iife: true
-  }
-};
-
 function jsMinification(config) {
+  var uglifyOptions = require(path.resolve('config/uglifyjs'));
   config.plugins.add(new UglifyJsPlugin(uglifyOptions));
   return config;
 };
